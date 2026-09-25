b58e736d422019c17df0dfe310f5f03f17de1377
Artera-alt <dulepovartem0@gmail.com>
2026-09-25 10:38:22 +0300

    Первый коммит: создание проекта

---
+++ Практика ASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Практика_ASP.NET.Models;
+
+namespace Практика_ASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contacs()
+        {
+            return View();
+        }
+
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
+        public IActionResult Workforce()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
